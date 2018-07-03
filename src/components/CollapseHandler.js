dab3ab1d844f6f51bf09210a00400e1ce5b168e2
ciruss <johanlaas4@gmail.com>
2018-07-03 09:58:19 +0300

    added collapse component, not working atm

---
+++ src/components/CollapseHandler.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Collapse from 'reactstrap/lib/Collapse';
+import ListGroupItem from 'reactstrap/lib/ListGroupItem';
+import PropTypes from 'prop-types';
+
+import CurrentWeather from './CurrentWeather';
+
+const APIUrl = 'http://api.openweathermap.org/data/2.5/';
+const APIKey = 'appid=51ac1e71f3bb963bdf6c1efe8dd0e33a';
+
+class CollapseHandler extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpened: false,
+			weatherId: '',
+			temp: '',
+			description: '',
+			wind: '',
+			humidity: '',
+		};
+		this.toggle = this.toggle.bind(this);
+		this.getCurrentWeather = this.getCurrentWeather.bind(this);
+	}
+
+	toggle() {
+		this.setState({ isOpened: !this.state.isOpened });
+	}
+
+	getCurrentWeather(value) {
+		return fetch(`${APIUrl}/weather?q=${value}&${APIKey}&units=metric`)
+			.then(response => {
+				if(!response.ok) {
+					throw Error(response.status);
+				}
+				return response.json();
+			})
+			.then(data => {
+				this.setState({
+					weatherId: data.weather[0].icon,
+					temp: data.main.temp,
+					description: data.weather[0].description,
+					humidity: data.main.humidity,
+					wind: data.wind.speed,
+				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	render() {
+		const { temp,
+			weatherId,
+			description,
+			wind,
+			humidity,
+		} = this.state;
+		const favorite = this.props;
+		return (
+			<ListGroupItem>
+				<div>
+					<p onClick={this.toggle}>
+						<strong>favorite</strong>
+					</p>
+					<Collapse
+						isOpen={this.state.isOpened}
+						onClick={this.getCurrentWeather(this.props.favorite)}
+					>
+						<CurrentWeather
+							cityName={favorite}
+							temp={temp}
+							weatherId={weatherId}
+							description={description}
+							wind={wind}
+							humidity={humidity}
+						/>
+					</Collapse>
+				</div>
+			</ListGroupItem>
+		);
+	}
+}
+
+CollapseHandler.propTypes = {
+	favorite: PropTypes.string.isRequired
+};
+
+export default CollapseHandler;
--- src/components/FavoritesTable.js
+++ src/components/FavoritesTable.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 import { Collapse } from 'react-collapse';
-import CurrentWeather from './CurrentWeather';
 
-const APIUrl = 'http://api.openweathermap.org/data/2.5/';
-const APIKey = 'appid=51ac1e71f3bb963bdf6c1efe8dd0e33a';
+import CollapseHandler from './CollapseHandler';
 
 class FavoritesTable extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+	state = {
 			favorites: JSON.parse(localStorage.getItem('favoriteCitys')) || [],
-			weatherId: '',
-			cityName: '',
-			temp: '',
-			description: '',
-			wind: '',
-			humidity: '',
-			isOpened: false,
-		};
-		this.getCurrentWeather = this.getCurrentWeather.bind(this);
 	}
 
 	componentDidUpdate(prevState) {
@@ -34,64 +21,11 @@ class FavoritesTable extends Component {
 		this.setState({ favorites: updatedList });
 	}
 
-	getCurrentWeather(value) {
-		return fetch(`${APIUrl}/weather?q=${value}&${APIKey}&units=metric`)
-			.then(response => {
-				if(!response.ok) {
-					throw Error(response.status);
-				}
-				return response.json();
-			})
-			.then(data => {
-				this.setState({
-					weatherId: data.weather[0].icon,
-					cityName: data.name,
-					temp: data.main.temp,
-					description: data.weather[0].description,
-					humidity: data.main.humidity,
-					wind: data.wind.speed,
-				});
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}
-
-	handleClick() {
-		this.setState(prevState => ({
-			isOpened: !prevState.isOpened
-		}));
-	}
-
 	render() {
-		const { temp,
-			cityName,
-			weatherId,
-			description,
-			wind,
-			humidity,
-		} = this.state;
 		return (
 			<div className='favorites'>
 				{this.state.favorites.map((city, index) =>
-					<span
-						className='favorites'
-						key={index}
-						onClick={() => this.getCurrentWeather(city)}
-					>
-						{city}
-						<button onClick={() => this.deleteItem(city)}>-</button>
-						<div className='favorites__weather'>
-							<CurrentWeather
-								cityName={cityName}
-								temp={temp}
-								weatherId={weatherId}
-								description={description}
-								wind={wind}
-								humidity={humidity}
-							/>
-						</div>
-					</span>
+					<CollapseHandler key={index} favorite={city} />
 				)}
 			</div>
 		);

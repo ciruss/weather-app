cf9b2124fdafd1a53d2f2e3ce3d19fcba7ec0674
ciruss <johanlaas4@gmail.com>
2019-08-12 23:51:13 +0300

    temperature switch WIP

--- src/Header.js
+++ src/Header.js
@@ -27,11 +27,58 @@ const FavoritesLink = styled.span`
 
 const SearchContainer = styled.div``;
 
+const SwitchWrapper = styled.div`
+    position: relative;
+`;
+
+const SwitchLabel = styled.label`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 42px;
+    height: 26px;
+    border-radius: 0;
+    background: #bebebe;
+    cursor: pointer;
+    &::after {
+        content: '';
+        display: block;
+        border-radius: 0;
+        width: 18px;
+        height: 18px;
+        margin: 3px;
+        background: #ffffff;
+        box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+        transition: 0.2s;
+        background: #4fbe79;
+    }
+`;
+
+const Switch = styled.input`
+    opacity: 0;
+    z-index: 1;
+    border-radius: 0;
+    width: 42px;
+    height: 26px;
+    &:checked + ${SwitchLabel} {
+        /* background: #4fbe79; */
+        &::after {
+            content: '';
+            display: block;
+            border-radius: 0;
+            width: 18px;
+            height: 18px;
+            margin-left: 21px;
+            transition: 0.2s;
+        }
+    }
+`;
+
 const Header = () => {
     const { isMetric, setIsMetric } = React.useContext(Context);
 
-    const changeUnits = () => {
-        setIsMetric(!isMetric);
+    const changeUnits = e => {
+        setIsMetric(e.target.checked);
     };
 
     return (
@@ -45,7 +92,15 @@ const Header = () => {
             <SearchContainer>
                 <SearchBox />
             </SearchContainer>
-            <button onClick={changeUnits}>Change to {isMetric ? 'Farenheit' : 'Metric'}</button>
+            <SwitchWrapper>
+                <Switch
+                    id="temperature-switch"
+                    type="checkbox"
+                    checked={isMetric}
+                    onChange={changeUnits}
+                />
+                <SwitchLabel htmlFor="temperature-switch" />
+            </SwitchWrapper>
         </StyledHeader>
     );
 };
